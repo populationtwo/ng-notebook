d5198a670b061ddc11c16f3626c939929fcddf67
populationtwo <populationtwo@gmail.com>
2016-02-11 21:37:46 -0500

    Add click handler to select note.

--- js/editor.js
+++ js/editor.js
@@ -6,6 +6,11 @@ angular
 function EditorController($scope, $http) {
     $scope.editing = true;
 
+    $scope.view = function(index){
+        $scope.editing = false;
+        $scope.content = $scope.notes[index];
+    };
+
     $http.get('/notes').success(function (data) {
         $scope.notes = data;
     }).error(function (err) {
