54b3c69493413a6c4f68261c052b6f77bf7d631a
populationtwo <populationtwo@gmail.com>
2016-02-11 22:04:40 -0500

    Create  event handler for save and delete.

--- js/editor.js
+++ js/editor.js
@@ -19,6 +19,46 @@ function EditorController($scope, $http) {
         };
     };
 
+    $scope.save = function () {
+        $scope.content.date = new Date();
+
+        if ($scope.content.id) {
+            $http.put('/notes/' + $scope.content.id, $scope.content).success(function (data) {
+                $scope.editing = false;
+            }).error(function (err) {
+                $scope.error = 'Could not update note';
+            });
+        } else {
+            $scope.content.id = Date.now();
+            $http.post('/notes', $scope.content).success(function (data) {
+                $scope.notes.push($scope.content);
+                $scope.editing = false;
+            }).error(function (err) {
+                $scope.error = 'Could not create note';
+            });
+        }
+    };
+
+    $scope.remove = function () {
+        $http.delete('/notes/' + $scope.content.id).success(function (data) {
+            var found = -1;
+            angular.forEach($scope.notes, function (note, index) {
+                if (note.id === $scope.content.id) {
+                    found = index;
+                }
+            });
+            if (found >= 0) {
+                $scope.notes.splice(found, 1);
+            }
+            $scope.content = {
+                title: '',
+                content: ''
+            };
+        }).error(function (err) {
+            $scope.error = 'Could not delete note';
+        });
+    };
+
     $http.get('/notes').success(function (data) {
         $scope.notes = data;
     }).error(function (err) {
