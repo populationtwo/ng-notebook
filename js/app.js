16d73f8030267da6421b84e41a84bbbd22d55e7a
populationtwo <populationtwo@gmail.com>
2016-02-11 21:46:40 -0500

    Create markdown directive.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 angular
-    .module('App', []);
+    .module('App', [])
+    .directive('markdown', markdown);
+
+function markdown() {
+    var converter = new Showdown.converter();
+    return {
+        scope: {
+            markdown: '@'
+        },
+        link: function (scope, element, attrs) {
+            scope.$watch('markdown', function () {
+                var content = converter.makeHtml(attrs.markdown);
+                element.html(content);
+            });
+        }
+    }
+};
 
 $(document).foundation();
